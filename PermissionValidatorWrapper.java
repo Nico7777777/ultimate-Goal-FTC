d1bc0f8533777f1f938f73335aed69dd27239f91
Nicola Andrei-George <56604584+Nico7777777@users.noreply.github.com>
2023-09-25 23:19:45 +0300

    removed useless stuff

--- PermissionValidatorWrapper.java
+++ PermissionValidatorWrapper.java
@@ -1,42 +1,10 @@
-/*
- * Copyright (c) 2018 Craig MacFarlane
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * (subject to the limitations in the disclaimer below) provided that the following conditions are
- * met:
- *
- * Redistributions of source code must retain the above copyright notice, this list of conditions
- * and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
- * and the following disclaimer in the documentation and/or other materials provided with the
- * distribution.
- *
- * Neither the name of Craig MacFarlane nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS
- * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package org.firstinspires.ftc.robotcontroller.internal;
 
 import android.Manifest;
 import android.os.Bundle;
-
 import com.qualcomm.ftcrobotcontroller.R;
-
 import org.firstinspires.ftc.robotcore.internal.system.Misc;
 import org.firstinspires.ftc.robotcore.internal.system.PermissionValidatorActivity;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,9 +12,6 @@ public class PermissionValidatorWrapper extends PermissionValidatorActivity {
 
     private final String TAG = "PermissionValidatorWrapper";
 
-    /*
-     * The list of dangerous permissions the robot controller needs.
-     */
     protected List<String> robotControllerPermissions = new ArrayList<String>() {{
         add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         add(Manifest.permission.READ_EXTERNAL_STORAGE);
